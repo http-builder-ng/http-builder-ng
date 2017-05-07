ca3f9334c0c4a1b54efa544e687a1f21b5e15a3c
cjstehno <chris@stehno.com>
2017-05-07 08:11:19 -0500

    Modified the copyright header to be attributed to the project rather than Dave - Dave approved. :-)

--- http-builder-ng-core/src/main/resources/59f7b2e5d5a78b25c6b21eb3b6b4f9ff77d11671.groovy
+++ http-builder-ng-core/src/main/resources/59f7b2e5d5a78b25c6b21eb3b6b4f9ff77d11671.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 David Clark
+ * Copyright (C) 2017 HttpBuilder-NG Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import groovyx.net.http.NativeHandlers
 
 import java.nio.charset.StandardCharsets
